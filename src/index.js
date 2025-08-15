f98436f848cb552aeead320fc86fcfe31391b8b9
anshitgupta007 <anshitg365@gmail.com>
2025-08-15 19:45:49 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ dotenv.config(
     path : "./.env"
    }
 );
-connectDB();
+connectDB().then(()=>{
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`);
+    });
+}).catch((err)=>{
+    console.error("Database connection failed:", err);
+})
